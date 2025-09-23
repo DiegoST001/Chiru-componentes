587b0322b14ad4e7d820c90480ba183ad422487c
Marcos Chocce <fernando.chocce@tecsup.edu.pe>
2025-09-23 15:16:37 -0500

    Add template Suplier info

---
+++ src/components/templates/TemplateSuplierInfo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../organisms/Header";
+import { HeaderCompany } from "../organisms/HeaderCompany";
+import { HeaderCompanyProfile } from "../organisms/HeaderCompanyProfile";
+
+const TemplateSupplierInfo = () => {
+  return (
+    <div>
+      <Header />
+      <HeaderCompanyProfile />
+    </div>
+  );
+};
+
+export default TemplateSupplierInfo;
